1619fe9fdc2a1a5e1406d3ee07d0f41ebb0c233e
exabugs <exabugs@gmail.com>
2014-08-18 00:02:19 +0900

    first commit.

--- test/server_test.js
+++ test/server_test.js
@@ -1,17 +1,89 @@
 "use strict";
 
 var Trigger = require('../lib/trigger');
+var http = require('http');
+var should = require('should');
 
-describe('FileableBase', function () {
+var port = process.env.PORT || 8888;
+
+describe('Trigger', function () {
+
+  var trigger_A = new Trigger();
+
 
   it('Prepare.', function (done) {
 
-    var trigger = new Trigger();
 
-    trigger.connect('localhost:8888');
+    var plainHttpServer = http.createServer(function (req, res) {
+    }).listen(port, function () {
 
-    trigger.on('message', function (msg) {
-      console.log(msg);
+      trigger_A.listen(this);
+
+      /*
+      trigger_A.on('message', function (msg) {
+        console.log(msg);
+      });
+*/
+      done();
+
+    });
+
+  });
+
+
+  it('trigger_B.', function (done) {
+
+    var trigger_B = new Trigger();
+
+    trigger_B.connect(['localhost', port].join(':'), function (err) {
+
+      trigger_B.on('message', function (msg) {
+        console.log(msg);
+        msg.should.eql('hello');
+
+        trigger_B.close(function (err) {
+          done();
+        });
+      });
+
+      trigger_A.send('hello');
+
+    });
+
+  });
+
+  it('trigger_C.', function (done) {
+
+    var trigger_C = new Trigger();
+    var trigger_D = new Trigger();
+
+    var count = 0;
+
+    trigger_C.connect(['localhost', port].join(':'), function (err) {
+      trigger_D.connect(['localhost', port].join(':'), function (err) {
+
+        trigger_C.on('message', function (msg) {
+          console.log(msg);
+          msg.should.eql('hello');
+          _done();
+        });
+
+        trigger_D.on('message', function (msg) {
+          console.log(msg);
+          msg.should.eql('hello');
+          _done();
+        });
+
+        trigger_A.send('hello');
+
+        function _done() {
+          count++;
+          if (1 < count) {
+            done();
+          }
+        }
+
+      });
     });
 
   });
